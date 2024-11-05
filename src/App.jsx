4b1eb651d44a4c88c394cc3c877efc272ae54f6d
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2024-11-05 12:26:41 -0500

    Added navbar to app and added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import Auth from './components/auth';
+import Navbar from './components/navbar';
 import './app.css';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
